54a9217750331dc74fa80ea0c3730f72887db274
GW <kowonp@gmail.com>
2021-10-02 04:13:51 +0900

    Class on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
-interface Person {
-  name: string;
-  age: number;
-  gender: string;
+import * as CryptoJS from "crypto-js";
+
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  static calculateHash = (index: number, previousHash: string, data: string, timestamp: number): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data);
+
+  constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
-const person = {
-  name: "GW",
-  age: 20,
-  gender: "male",
-};
+const firstBlock: Block = new Block(0, "1fs2eacasd", "", "hello", 123456);
 
-const handlePerson = (person: Person): void => {
-  console.log(person.name, person.age, person.gender);
-};
+let blockchain: Block[] = [firstBlock];
+console.log("blockchain", blockchain);
 
-handlePerson(person);
+const getBlockchain = (): Block[] => blockchain;
+
+const getLastBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimestamp = (): number => Math.round(Date.now() / 1000);
 
 export {};
